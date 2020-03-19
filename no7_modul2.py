d0510b294434d4ae9b11135d9daf1fb0521fbab7
L200180125 <48276941+L200180125@users.noreply.github.com>
2020-03-19 19:42:27 +0700

    Add files via upload

---
+++ no7_modul2.py
@@ -0,0 +1,20 @@
+#SOAL NO7
+#NIM berasal dari class mahasiswa
+#ambilNIM berasal dari class mahasiswa
+#ambilNama berasal dari class mahasiswa
+#ambilUangSaku berasal dari class mahasiswa
+#katakanPy berasal dari class mhsTIF
+#keadaan berasal dari class Manusia
+#kotaTinggal berasal dari class mahasiswa
+#makan berasal dari class Manusia
+#mengalikanDenganDua berasal dari class mahasiswa
+#nama berasal dari class mahasiswa dan manusia
+#listKuliah berasal dari class Mahasiswa
+#ambilKuliah berasal dari class mahasiswa
+#hapusKuliah berasal dari class mahasiswa
+#tambahUangSaku berasal dari class mahasiswa
+#UangSaku berasal dari class mahasiswa
+#ambilKotaTingal berasal dari class mahasiswa
+#olaheaga berasal dari class mahasiswa
+#perbaruiKotaTinggal berasal dari class mahasiswa
+#ucapKataSalam berasal dari class manusia
